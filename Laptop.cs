ca2ee29dd2745ee172dbf95f6e1dbca883766f1d
Hema123kamadolli <hema.kamadolli@mphasis.com>
2023-11-22 16:10:43 +0530

    Initial commit

---
+++ Laptop.cs
@@ -0,0 +1,47 @@
+﻿using System;
+ 
+
+namespace MultipleInterface
+{
+    internal class Laptop : IConnectable, IDisplayable, IRechargeable
+    {
+        public bool IsConnected { get; set; }
+        public string Name { get; set; }
+        public int Battery { get; set; }
+
+        public Laptop()
+        {
+            IsConnected = false;
+            Battery = 0;
+        }
+
+        public bool Connect()
+        {
+            IsConnected = true;
+            return true;
+
+        }
+        public int Charge(int min)
+        {
+            Battery = min;
+            Battery = Battery / 60;
+            return Battery;
+        }
+
+        public void Display()
+        {
+            if (IsConnected == true)
+            {
+                Console.WriteLine("Laptop is Connected");
+
+            }
+            else
+            {
+                Console.WriteLine("Laptop is not Connected");
+            }
+            Console.WriteLine($"{Battery} hours  is required to charge");
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+ 
+
+namespace MultipleInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Smartphone Electronic = new Smartphone();
+            Electronic.Connect();
+            Electronic.Charge(100);
+            Electronic.Display();
+
+            Laptop laptop = new Laptop();
+            laptop.Connect();
+            laptop.Charge(120);
+            laptop.Display();
+            Console.ReadKey();
+
+        }
+    }
+}
